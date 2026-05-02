9f97708caad15ecaddb96e413d6398db5d432977
SkwidMaster <c27william.kozma@afacademy.af.edu>
2026-05-02 12:37:39 -0600

    So many changes at this point

--- src/controller_fsm.vhd
+++ src/controller_fsm.vhd
@@ -22,22 +22,56 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
+--entity controller_fsm is
+--    Port ( i_reset : in STD_LOGIC;
+--           i_adv : in STD_LOGIC;
+--           o_cycle : out STD_LOGIC_VECTOR (3 downto 0));
+--end controller_fsm;
+
 entity controller_fsm is
-    Port ( i_reset : in STD_LOGIC;
-           i_adv : in STD_LOGIC;
-           o_cycle : out STD_LOGIC_VECTOR (3 downto 0));
+    Port (
+        i_clk  : in std_logic;
+        i_reset: in std_logic;
+        i_adv  : in std_logic;
+        o_cycle: out std_logic_vector(3 downto 0)
+    );
 end controller_fsm;
 
+--architecture FSM of controller_fsm is
+--    signal state : STD_LOGIC_VECTOR(3 downto 0) := "0001";
+--begin
+
+--    process(i_adv)
+--    begin
+--        if rising_edge(i_adv) then
+--            if i_reset = '1' then
+--                state <= "0001";
+--            else
+--                case state is
+--                    when "0001" => state <= "0010";
+--                    when "0010" => state <= "0100";
+--                    when "0100" => state <= "1000";
+--                    when "1000" => state <= "0001";
+--                    when others => state <= "0001";
+--                end case;
+--            end if;
+--        end if;
+--    end process;
+
+--    o_cycle <= state;
+
+--end FSM;
+
 architecture FSM of controller_fsm is
-    signal state : STD_LOGIC_VECTOR(3 downto 0) := "0001";
+    signal state : std_logic_vector(3 downto 0) := "0001";
 begin
 
-    process(i_adv)
+    process(i_clk)
     begin
-        if rising_edge(i_adv) then
+        if rising_edge(i_clk) then
             if i_reset = '1' then
                 state <= "0001";
-            else
+            elsif i_adv = '1' then
                 case state is
                     when "0001" => state <= "0010";
                     when "0010" => state <= "0100";
--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -47,6 +47,10 @@ architecture top_basys3_arch of top_basys3 is
     signal w_result : std_logic_vector(7 downto 0);
     signal w_flags : std_logic_vector(3 downto 0);
     signal w_cycle : std_logic_vector(3 downto 0);
+    
+    signal w_result_reg : std_logic_vector(7 downto 0); -- used for displaying to the 7-seg display
+signal bcd_tens  : std_logic_vector(3 downto 0);
+signal bcd_ones  : std_logic_vector(3 downto 0);
 
     -- 7-seg signals
     signal refresh_counter : unsigned(19 downto 0) := (others => '0');
@@ -55,6 +59,9 @@ architecture top_basys3_arch of top_basys3 is
 
     -- decoder output
     signal seg_internal : std_logic_vector(6 downto 0);
+    
+    signal btnC_sync : std_logic_vector(1 downto 0) := "00";
+    signal btnC_pulse : std_logic;
 
     -- component declaration
     component sevenseg_decoder
@@ -71,11 +78,12 @@ begin
 
     -- FSM
     FSM_inst : entity work.controller_fsm
-        port map (
-            i_reset => btnU,
-            i_adv   => btnC,
-            o_cycle => w_cycle
-        );
+    port map (
+        i_clk   => clk,
+        i_reset => btnU,
+        i_adv   => btnC_pulse,
+        o_cycle => w_cycle
+    );
 
     -- ALU
     ALU_inst : entity work.ALU
@@ -90,10 +98,52 @@ begin
     -- LEDs
     led(3 downto 0) <= w_cycle;
     led(15 downto 12) <= w_flags;
-    led(11 downto 4) <= w_result;
+--    led(11 downto 4) <= w_result_reg;
     
     
     -- 7-SEGMENT USING DECODER
+process(w_result_reg)
+    variable bin   : unsigned(7 downto 0);
+    variable bcd   : unsigned(11 downto 0); -- tens + ones
+    variable i     : integer;
+begin
+    bin := unsigned(w_result_reg);
+    bcd := (others => '0');
+
+    for i in 0 to 7 loop
+        -- shift left BCD + input
+        bcd := bcd(10 downto 0) & bin(7);
+        bin := bin(6 downto 0) & '0';
+
+        -- add 3 if needed
+        if bcd(3 downto 0) > 4 then
+            bcd(3 downto 0) := bcd(3 downto 0) + 3;
+        end if;
+
+        if bcd(7 downto 4) > 4 then
+            bcd(7 downto 4) := bcd(7 downto 4) + 3;
+        end if;
+
+        if bcd(11 downto 8) > 4 then
+            bcd(11 downto 8) := bcd(11 downto 8) + 3;
+        end if;
+    end loop;
+
+    bcd_ones <= std_logic_vector(bcd(3 downto 0));
+    bcd_tens <= std_logic_vector(bcd(7 downto 4));
+end process;
+
+-- update the visible register
+process(clk)
+begin
+    if rising_edge(clk) then
+        if btnU = '1' then
+            w_result_reg <= (others => '0');
+        elsif btnC_pulse = '1' then
+            w_result_reg <= w_result;
+        end if;
+    end if;
+end process;
 
     -- clock divider
     process(clk)
@@ -102,19 +152,29 @@ begin
             refresh_counter <= refresh_counter + 1;
         end if;
     end process;
+    
+    process(clk)
+begin
+    if rising_edge(clk) then
+        btnC_sync(0) <= btnC;
+        btnC_sync(1) <= btnC_sync(0);
+    end if;
+end process;
+
+btnC_pulse <= btnC_sync(0) and not btnC_sync(1);
 
     digit_select <= refresh_counter(19);
 
     -- choose digit
     process(digit_select, w_result)
     begin
-        if digit_select = '0' then
-            an <= "1110"; -- rightmost
-            current_nibble <= w_result(3 downto 0);
-        else
-            an <= "1101"; -- next digit
-            current_nibble <= w_result(7 downto 4);
-        end if;
+if digit_select = '0' then
+    an <= "1110";
+    current_nibble <= bcd_ones;
+else
+    an <= "1101";
+    current_nibble <= bcd_tens;
+end if;
     end process;
 
     -- instantiate decoder
